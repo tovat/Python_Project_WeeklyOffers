11ff76c093f1912d212abf7411f78cfb419a132e
tovat <147144672+tovat@users.noreply.github.com>
2024-09-26 15:23:51 +0200

    updated batch script to run main script as a module

---
+++ run_weekly.bat
@@ -0,0 +1,6 @@
+@echo off
+
+cd /d "C:\Users\tovat\OneDrive\Dokument\EC_utbildning\Python\Python_Project_WeeklyOffers"
+
+"C:\Users\tovat\OneDrive\Dokument\EC_utbildning\Python\Python_Project_WeeklyOffers\.venv\Scripts\python.exe" -m fetch_weekly_offers.run_weekly
+
